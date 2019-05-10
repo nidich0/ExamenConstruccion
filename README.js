c10c6b521414590defec97c642b599d84ffa23af
amfiis1@gmail.com <amfiis1@gmail.com>
2019-05-10 10:46:57 -0500

    mi primer commit de mi rama1

---
+++ README.js
@@ -0,0 +1,30 @@
+# ExamenRama1
+import javax.swing.Rama1;
+
+public class arbol{
+  private String codigo;
+  private String altura;
+  private String tipo;
+  private String longitud;
+  private String latitud;
+
+  public arbol(){
+    this.arbol="Tamaño desconocido.";
+  }
+
+  public arbol(String codigo,String altura,String tipo,String longitud,String fechaNacimiento,String sexo)
+        this.longitud="s/m";
+}
+  
+   //imprimir clase completo
+  public String toString() {
+      return "codigo: "+this.codigo, "altura: "+this.altura,"tipo"+ this.tipo,"longitud: "+this.longitud,
+      "lontitud: "+this.longitud;
+  }
+
+}
+
+
+}
+
+
